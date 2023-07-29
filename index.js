12098aafd3e21369c1b79685059398305e2d1bf3
Ayokunnumi1 <omololuayk@gmail.com>
2023-07-30 00:46:23 +0100

    Add Event Listener, create & style buttons

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const divContainer = document.querySelector('.div-container');
+const firstSmallDiv = document.querySelector('.first-small-div');
 
 
-
-for (let i = 0; i < 204; i++) {
+for (let i = 0; i < 315; i++) {
     const firstSmallDiv = document.createElement('div');
     firstSmallDiv.setAttribute('class', 'first-small-div');
     divContainer.appendChild(firstSmallDiv);
-    console.log(i)
+    // console.log(i)
+    firstSmallDiv.addEventListener('mouseenter', () => {
+        firstSmallDiv.style.backgroundColor = 'black';        
+    });
+    
+    firstSmallDiv.addEventListener('mouseleave', () => {
+        firstSmallDiv.style.backgroundColor = ' ';
+    });
 }
-// firstSmallDiv.textContent = 'HII'
+
+
+
+
